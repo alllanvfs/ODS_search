69d0933f3ae4e31528c1ca66286fc0d43c3c713b
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2025-02-06 20:50:41 -0300

    corrigindo wquebra das box

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,17 +1,14 @@
 function filtrarOng() {
     var input = document.getElementById("ong-input").value.toLowerCase();
-    
     var ongs = document.getElementsByClassName("ong");
-    
+
     for (var i = 0; i < ongs.length; i++) {
         var ong = ongs[i];
-        
+
         if (ong.textContent.toLowerCase().includes(input)) {
             ong.classList.remove("oculto");
-            ong.classList.add("centro");
         } else {
             ong.classList.add("oculto");
-            ong.classList.remove("centro");
         }
     }
 }
